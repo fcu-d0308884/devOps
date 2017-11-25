491be9903969028f529b5333923a76dbd3d36a89
d0308884 <zh3635897@gmail.com>
2017-11-25 14:24:15 +0800

    add

--- main.go
+++ main.go
@@ -2,8 +2,10 @@ package main
 
 import (
     "net/http"
-    
+    "strconv"
+
     "github.com/labstack/echo"
+
 )
 
 func main() {
@@ -11,5 +13,25 @@ func main() {
     e.GET("/", func(c echo.Context) error {
         return c.String(http.StatusOK, "Hello, World!")
     })
-    e.Logger.Fatal(e.Start(":1323"))
+ 
+	
+	 e.GET("/add/:num1/:num2", func(c echo.Context) error {
+        num1Str := c.Param("num1")
+        num2Str := c.Param("num2")
+        num1, err := strconv.Atoi(num1Str)
+        if err != nil {
+            return c.String(http.StatusUnprocessableEntity, err.Error())
+        }
+        num2, err := strconv.Atoi(num2Str)
+        if err != nil {
+            return c.String(http.StatusUnprocessableEntity, err.Error())
+        }
+        result := Add(num1, num2)
+        return c.String(http.StatusOK, strconv.Itoa(result))
+    })   
+	
+	e.Logger.Fatal(e.Start(":1234"))
+}
+func Add(num1, num2 int) int {
+    return num1 + num2
 }
